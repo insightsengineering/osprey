2e7ed71c38aebc50faa818e5f83d2f14c520f72d
Maekinen, Mika {MDBB~Basel} <mika.maekinen@roche.com>
2018-02-15 17:07:12 +0100

    adding dummyplot

---
+++ R/dummyplot.R
@@ -0,0 +1,34 @@
+
+
+#' Create a dummyplot
+#'
+#' Descr of this plot
+#'
+#' @param x x coordinates
+#' @param y y coordinates
+#' @param col color of points
+#'
+#' @return ggplot object
+#'
+#' @export
+#'
+#' @author Mika
+#'
+#' @examples
+#' library(random.cdisc.data)
+#'
+#'
+#' ASL <- radam("ASL", start_with = list(WEIGHT = c(80, 85, 90), STUDYID = "A"))
+#'
+#' with(ASL, spiderplot(WEIGHT, BAGE, ARM))
+#'
+#' spiderplot(x = ASL$WEIGHT, y = ASL$BAGE, col = ASL$ARM)
+#'
+#'
+dummyplot <- function(x, y, col) {
+
+  df <- data.frame(x = x, y = y, col = col)
+
+  df %>%  ggplot() + aes(x, y, color = col) + geom_point()
+
+}
