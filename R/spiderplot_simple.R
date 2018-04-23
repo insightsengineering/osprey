659d8944795ce3c9700098395b7e0c4d574f8586
Maekinen, Mika {MDBB~Basel} <mika.maekinen@roche.com>
2018-04-23 15:02:08 +0200

    added pider_simple with parameters

--- R/spiderplot_simple.R
+++ R/spiderplot_simple.R
@@ -4,38 +4,48 @@
 #'
 #' Descr of this plot
 #'
-#' @param anl Analysis dataset
+#' @param byvar Analysis dataset
+#' @param days Variable with time in days
+#' @param mesValue Variable with measurement
+#' @param groupCol Variable to color the individual lines and id in plot
+#' @param baseday Numeric Value, pts with only smaller values will be cut out
+#'
 #'
 #' @return ggplot object
 #'
 #' @export
 #'
-#' @author Mika
+#' @author Mika Maekinen
 #'
 #' @examples
 #' library(random.cdisc.data)
 #'
 #'
-#' atr <- radam("ATR", N=10 ,start_with = list(CHANGE = c(0, 80, 85, 90), STUDYID = c("A","B")))
+#' atr <- left_join(radam("ATR", N=10),radam("ADSL", N=10))
 #'
 #'
-#' spiderplot_simple(atr %>% filter(PARAMCD == "SUMTGLES"))
+#' spiderplot_simple(atr %>% filter(PARAMCD == "SUMTGLES"), groupCol=SEX)
 #'
-#'
-spiderplot_simple <- function(anl) {
 
+spiderplot_simple <- function(anl, byvar=USUBJID, days=TUDY, mesValue=PCHG, groupCol=USUBJID, baseday= 0) {
+  byvar <- enquo(byvar)
+  days <- enquo(days)
+  mesValue <- enquo(mesValue)
+  groupCol <- enquo(groupCol)
   ###remove patients without post baseline measurement
-  anl <- anl %>% group_by(USUBJID) %>% mutate(morebase = ifelse(max(TUDY, na.rm=TRUE) > 0, TRUE, FALSE)) %>% filter(morebase == TRUE) %>% ungroup()
+  anl <- anl %>% group_by(!! byvar) %>%
+    mutate(morebase = ifelse(max(!! days, na.rm=TRUE) > baseday, TRUE, FALSE)) %>%
+    filter(morebase == TRUE) %>% ungroup()
   ###find the last measurement
-  lastObs <- anl  %>%  group_by(USUBJID) %>% slice(which.max(TUDY))
+  lastObs <- anl  %>%  group_by(!! byvar) %>% slice(which.max(!! days))
 
-  ##plotr
-    ggplot(data=anl, mapping = aes(x = TUDY, y = PCHG, group = USUBJID, colour=USUBJID),size=2, alpha = 1) +
-    geom_point(size=3) + geom_line(size=2, alpha=0.7) +
-    geom_text(aes(x = TUDY, y = PCHG, label=USUBJID), data=lastObs, hjust = 0) +
-    geom_hline(aes(yintercept = 0), linetype="dotted" , color = "black") +
-    xlab("Time (Days)") +
-    ylab("Change(%) from Baseline") +
-    expand_limits(x = anl$TUDY * 1.2)
+  #plotr
+  ggplot(data=anl, mapping = aes_(x = days, y = mesValue, group = byvar, colour= groupCol),size=2, alpha = 1) +
+  geom_point(size=3) + geom_line(size=2, alpha=0.7) +
+  geom_text(aes_(x = days, y =  mesValue, label= byvar), data=lastObs, hjust = 0) +
+  geom_hline(aes(yintercept = 0), linetype="dotted" , color = "black") +
+  xlab("Time (Days)") +
+  ylab("Change(%) from Baseline") +
+  expand_limits(anl, x = select_(anl, mesValue) * 1.2)
 }
 
