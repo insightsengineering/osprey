859e3e4bd76aa61cc7a36a2eda705a05de5f94b9
Zhang, Carolyn {MDBB~South San Francisco} <zhang.carolyn@gene.com>
2018-06-13 01:21:12 +0200

    include options for discontinued study marker in spiderplot

--- R/g_spiderplot.R
+++ R/g_spiderplot.R
@@ -8,11 +8,13 @@
 #' @param marker_color vector defines by what variable points are color coded,
 #' , default here is NULL
 #' @param marker_color_opt vector defines marker color code, default here is NULL
+#' @param marker_size size of markers in plot
 #' @param marker_shape vector defines by what variable points are shape coded,
 #' , default here is NULL
 #' @param marker_shape_opt vector defines marker shape code, default here is NULL
-#' @param line_color vector defines by what variable plot is color coded
-#' @param datalabel_txt vector defines text at last time point for each line
+#' @param line_color_colby vector defines by what variable plot is color coded
+#' @param datalabel_txt list defines text (at last time point) and flag for discontinued study
+#' (per defined variable) - can be list of length 1 or 3
 #' @param facet_rows vector defines what variable is used to split the
 #' plot into rows, default here is NULL
 #' @param facet_columns vector defines what variable is used to split the
@@ -48,27 +50,29 @@
 #'              y_label = "Change (%) from Baseline",
 #'              marker_x = dat$TUDY,
 #'              marker_y = dat$PCHG,
-#'              marker_color = dat$RACE,
-#'              marker_color_opt = map_marker_color,
+#'              marker_color = dat$USUBJID,
+#'              #marker_color_opt = map_marker_color,
 #'              marker_shape = dat$RACE,
-#'              marker_shape_opt = map_marker_shape,
-#'              line_color = dat$USUBJID,
-#'              datalabel_txt = lbl,
+#'              #marker_shape_opt = map_marker_shape,
+#'              marker_size = 5,
+#'              line_color_colby = dat$USUBJID,
+#'              datalabel_txt = list(one = dat$USUBJID, two = dat$USUBJID, three = c("id-2", "id-4", "id-7")),
 #'              facet_rows = dat$SEX,
 #'              facet_columns = dat$ARM,
 #'              vref_line = c(10, 37),
 #'              href_line = -0.3,
 #'              show_legend = FALSE)
 #'
-g_spiderplot <- function(x_label,
-                         y_label,
+g_spiderplot <- function(x_label = "Time (Days)",
+                         y_label = "Change (%) from Baseline",
                          marker_x,
                          marker_y,
                          marker_color = NULL,
                          marker_color_opt = NULL,
                          marker_shape = NULL,
                          marker_shape_opt = NULL,
-                         line_color,
+                         marker_size = 6,
+                         line_color_colby = NULL,
                          datalabel_txt = NULL,
                          facet_rows = NULL,
                          facet_columns = NULL,
@@ -79,7 +83,8 @@ g_spiderplot <- function(x_label,
                          newpage = TRUE){
 
   #set up data-------
-  dat <- data.frame(day = marker_x, pchg = marker_y, l_col = line_color)
+  dat <- data.frame(day = marker_x, pchg = marker_y)
+
   if(!is.null(marker_color)){
     dat$m_col <- marker_color
   }
@@ -92,56 +97,96 @@ g_spiderplot <- function(x_label,
   if(!is.null(facet_columns)){
     dat$f_columns <- facet_columns
   }
+  if(!is.null(line_color_colby)){
+    dat$l_col <- line_color_colby
+  }
+  if(!is.null(datalabel_txt)){
+    cmp <- max(marker_x)
+    #dat_txt <- data.frame(day = marker_x, pchg = marker_y, lbl = datalabel_txt[[1]])
+    lbl <- apply(dat, 1, function(dat){if(dat[1] == cmp){dat[3]}else{""}})
+    dat$lab <- lbl
+  }
+  if(length(datalabel_txt) == 3){
+    dat$id <- datalabel_txt[[2]]
+  }
 
   #plot spider plot-----------------
-  pl <- ggplot(data = dat, aes(x = dat$day, y = dat$pchg)) +
-    geom_line(aes(color = dat$l_col), size = 2, alpha = 0.5, show.legend = FALSE)  +
+  pl <- ggplot(data = dat, aes(x = day, y = pchg)) +
     xlab(x_label) +
     ylab(y_label) +
     theme(legend.position="top", legend.title = element_blank())
 
+  if(!is.null(line_color_colby)){
+    pl <- pl + geom_line(aes(color = dat$l_col), size = 2, alpha = 0.5, show.legend = FALSE)
+  }
+  else{
+    pl <- pl + geom_line(size = 2, alpha = 0.5, show.legend = FALSE)
+  }
+
   #marker shape and color------------
   if(!is.null(marker_color) && !is.null(marker_shape)){
     if(!is.null(marker_color_opt) && !is.null(marker_shape_opt)){
-      pl <- pl + geom_point(aes(color = dat$m_col, shape = dat$sh), shape = marker_shape_opt, colour = marker_color_opt, size = 3, show.legend = show_legend)
+      pl <- pl + geom_point(aes(color = dat$m_col, shape = dat$sh), shape = marker_shape_opt, colour = marker_color_opt, size = marker_size, show.legend = show_legend)
     }
     else if(!is.null(marker_color_opt) && is.null(marker_shape_opt)){
-      pl <- pl + geom_point(aes(color = dat$m_col, shape = dat$sh), colour = marker_color_opt, size = 3, show.legend = show_legend)
+      pl <- pl + geom_point(aes(color = dat$m_col, shape = dat$sh), colour = marker_color_opt, size = marker_size, show.legend = show_legend)
     }
     else if(is.null(marker_color_opt) && !is.null(marker_shape_opt)){
-      pl <- pl + geom_point(aes(color = dat$m_col, shape = dat$sh), shape = marker_shape_opt, size = 3, show.legend = show_legend)
+      pl <- pl + geom_point(aes(color = dat$m_col, shape = dat$sh), shape = marker_shape_opt, size = marker_size, show.legend = show_legend)
     }
     else{
-      pl <- pl + geom_point(aes(color = dat$m_col, shape = dat$sh), size = 3, show.legend = show_legend)
+      pl <- pl + geom_point(aes(color = dat$m_col, shape = dat$sh), size = marker_size, show.legend = show_legend)
     }
   }
   else if(!is.null(marker_color) && is.null(marker_shape)){
     if(!is.null(marker_color_opt)){
-      pl <- pl + geom_point(color = marker_color_opt, size = 3, show.legend = show_legend)
+      pl <- pl + geom_point(color = marker_color_opt, size = marker_size, show.legend = show_legend)
     }
     else{
-      pl <- pl + geom_point(aes(color = dat$m_col), size = 3, show.legend = show_legend)
+      pl <- pl + geom_point(aes(color = dat$m_col), size = marker_size, show.legend = show_legend)
     }
-
   }
   else if(is.null(marker_color) && !is.null(marker_shape)){
     if(!is.null(marker_shape_opt)){
-      pl <- pl + geom_point(shape = marker_shape_opt, size = 3, show.legend = show_legend)
+      pl <- pl + geom_point(shape = marker_shape_opt, size = marker_size, show.legend = show_legend)
     }
     else{
-      pl <- pl + geom_point(aes(shape = dat$sh), size = 3, show.legend = show_legend)
+      pl <- pl + geom_point(aes(shape = dat$sh), size = marker_size, show.legend = show_legend)
     }
-
   }
   else if(is.null(marker_color) && is.null(marker_shape)){
     pl <- pl + geom_point(size = 3, show.legend = show_legend)
   }
 
-  #label location---------
+  #label at last data point---------
   if(!is.null(datalabel_txt)){
-    datalabel_txt_mod <- gsub(" ", "\n ", datalabel_txt, fixed = TRUE)
-    pl <- pl + geom_text(aes(x = dat$day+4, y =  dat$pchg, label= datalabel_txt_mod), size = 2)
-    pl <- pl + geom_text(aes(x = dat$day+(max(nchar(datalabel_txt_mod))/2), y =  dat$pchg+(max(dat$pchg)/10), label= ""), size = 2)
+    # cmp <- max(marker_x)
+    # #dat_txt <- data.frame(day = marker_x, pchg = marker_y, lbl = datalabel_txt[[1]])
+    # lbl <- apply(dat, 1, function(dat){if(dat[1] == cmp){dat[3]}else{""}})
+    # dat$lab <- lbl
+    #print(dat)
+    #print(lbl)
+    #dat_txt <- dat %>% mutate(lbl = if_else(day == cmp, id, ""))
+
+    #datalabel_txt_mod <- gsub(" ", "\n ", lbl, fixed = TRUE)
+    pl <- pl + geom_text(data = dat, aes(x = day+3, y =  pchg, label= lab), size = 3)
+    pl <- pl + geom_text(data = dat, aes(x = dat$day+max(nchar(lbl)), y =  dat$pchg+(max(dat$pchg)/10), label= ""), size = 6)
+
+    #pl <- pl + geom_text(data = dat_txt, mapping = aes(x = dat$day+3, y = dat$pchg, label = dat$id), size = 6)
+
+    if(length(datalabel_txt) == 2)
+      stop("Please input a list of length 3 to add arrow for annotation")
+    if(length(datalabel_txt) == 3){
+      dat_arrow <- dat %>%
+        filter(id %in% datalabel_txt[[3]]) %>%
+        filter(day == cmp)
+
+      #print(dat)
+      #print(dat_arrow)
+
+      #dat_arrow <- apply(dat_arr, 1, function(dat){if(dat["id"] %in% datalabel_txt[[3]]){TRUE}else{FALSE}})
+      pl <- pl + geom_segment(data = dat_arrow, mapping = aes(x = day-7, y = pchg, xend = day-2, yend = pchg), arrow = arrow(length = unit(0.25, "inches")), size = 1, color = "black")
+    }
   }
 
   if(!is.null(href_line)){
@@ -151,6 +196,7 @@ g_spiderplot <- function(x_label,
     pl <- pl + geom_vline(xintercept = vref_line, linetype = "dotted", color = "black")
   }
 
+  print(data.frame(dat$f_rows, dat$f_columns, dat$lab))
   #facets---------------
   if(is.null(facet_rows) && is.null(facet_columns)){
     pl
@@ -164,6 +210,10 @@ g_spiderplot <- function(x_label,
   else{
     pl <- pl + facet_grid(f_rows ~ f_columns)
   }
+
+  #change fontsize
+  pl <- pl + theme_linedraw() +
+    theme(strip.text = element_text(colour = "black"), strip.background = element_rect(colour = "white", fill = "white"), text = element_text(size = 25))
 
   pl
 
