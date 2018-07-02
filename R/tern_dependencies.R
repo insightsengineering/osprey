0dff60b714724a0e2fcf702f86dd5e9b47096f06
Zhang, Carolyn {MDBB~South San Francisco} <zhang.carolyn@gene.com>
2018-07-02 23:11:02 +0200

    tern depencency file

---
+++ R/tern_dependencies.R
@@ -0,0 +1,7 @@
+#import tern functions
+duplicate_with_var <- tern:::duplicate_with_var
+check_col_by <- tern:::check_col_by
+stack_rtables <- tern:::stack_rtables
+count_perc_col_N <- tern:::count_perc_col_N
+indent_table <- tern:::indent_table
+add_ae_class <- tern:::add_ae_class
