e23ee19bda0ac815a782db90d4186b816be89e45
Ondrej <ondrej.slama@roche.com>
2019-10-18 17:09:26 +0200

    removed rocheBCE dependency correctly placed ggplot2 in DESCRIPTION

--- R/tern_dependencies.R
+++ R/tern_dependencies.R
@@ -4,8 +4,4 @@ check_col_by <- getFromNamespace("check_col_by", "tern")
 count_perc_col_N <- getFromNamespace("count_perc_col_N", "tern") # nolint
 check_same_N <- getFromNamespace("check_same_n", "tern") # nolint
 to_n <- getFromNamespace("to_n", "tern")
-# t_max_grade_per_id <- getFromNamespace("t_max_grade_per_id", "tern")
 shift_label_table <- getFromNamespace("shift_label_table", "tern")
-
-
-
