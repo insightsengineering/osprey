858b3ae7c3231e52c5777e77f5e5eb84e485cfd1
Pawel <pawel.rucki@roche.com>
2020-12-07 19:31:59 +0100

    Fix with simultaneous plot facet and annotation labels

--- R/g_waterfall.R
+++ R/g_waterfall.R
@@ -142,7 +142,6 @@ g_waterfall <- function(bar_id,
                         ytick_at = 20,
                         y_label = "Best % Change from Baseline",
                         title = "Waterfall Plot") {
-
   # check data
   check_input_length <- c(nrow(data.frame(bar_id)), nrow(data.frame(bar_height)))
 
@@ -158,9 +157,9 @@ g_waterfall <- function(bar_id,
     } else {
       ls1 <- levels(factor(col_by))
       ls2 <- names(bar_color_opt)
-      if (length(intersect(ls1, ls2)) == 0){
+      if (length(intersect(ls1, ls2)) == 0) {
         stop("invalid argument: check that the col_by and bar_color_opt have overlapping categories")
-      } else if (length(ls2) < length(ls1)){
+      } else if (length(ls2) < length(ls1)) {
         stop("invalid argument: More categories in col_by than the ones listed in bar_color_opt")
       }
     }
@@ -198,7 +197,7 @@ g_waterfall <- function(bar_id,
         bar_data$bar_id,
         levels = unique(bar_data$bar_id[order(bar_data$sort_by, bar_data$bar_height, decreasing = TRUE)])
       )
-    } else{
+    } else {
       bar_data$bar_id <- factor(
         bar_data$bar_id,
         levels = unique(bar_data$bar_id[order(bar_data$bar_height, decreasing = TRUE)])
@@ -210,7 +209,7 @@ g_waterfall <- function(bar_id,
     bar_data <- bar_data %>% distinct(bar_id, .keep_all = TRUE)
 
     # plot bar plot
-    if (is.null(gap_point)){
+    if (is.null(gap_point)) {
       ybreaks <- seq(
         ytick_at * floor(min(bar_data$bar_height, na.rm = TRUE) / ytick_at),
         ytick_at * ceiling(max(bar_data$bar_height, na.rm = TRUE) / ytick_at),
@@ -230,7 +229,7 @@ g_waterfall <- function(bar_id,
           axis.title.x = element_blank()
         ) +
         ylab(y_label)
-    } else if (all(is.na(bar_height))){
+    } else if (all(is.na(bar_height))) {
       ybreaks <- seq(ytick_at * -2, ytick_at * 2, by = ytick_at)
 
       p <- ggplot(data = bar_data, aes(x = bar_id, y = bar_height)) +
@@ -247,7 +246,7 @@ g_waterfall <- function(bar_id,
           axis.title.x = element_blank()
         ) +
         ylab(y_label)
-    } else if (max(bar_height, na.rm = TRUE) <= gap_point)  {
+    } else if (max(bar_height, na.rm = TRUE) <= gap_point) {
       ybreaks <- seq(
         ytick_at * floor(min(bar_data$bar_height, na.rm = TRUE) / ytick_at),
         ytick_at * ceiling(max(bar_data$bar_height, na.rm = TRUE) / ytick_at),
@@ -271,14 +270,12 @@ g_waterfall <- function(bar_id,
       length_cut <- ytick_at *
         floor(0.8 * (min(bar_data$bar_height[bar_data$bar_height > gap_point], na.rm = TRUE) - gap_point) / ytick_at)
 
-      cut_fun <- function(x){
-        if (is.na(x)){
+      cut_fun <- function(x) {
+        if (is.na(x)) {
           x <- x
-        }
-        else if (x > gap_point){
+        } else if (x > gap_point) {
           x <- x - length_cut
-        }
-        else{
+        } else {
           x <- x
         }
       }
@@ -299,9 +296,9 @@ g_waterfall <- function(bar_id,
         ytick_at * ceiling(max(bar_data$new_bar_height, na.rm = TRUE) / ytick_at),
         by = ytick_at)
 
-      if (length(ylabel1) + length(ylabel2) == length(ybreaks)){
+      if (length(ylabel1) + length(ylabel2) == length(ybreaks)) {
         ylabels <- c(ylabel1, ylabel2)
-      } else{
+      } else {
         dif <- length(ylabel1) + length(ylabel2) - length(ybreaks)
         ylabel2 <- ylabel2[- (1:dif)]
         ylabels <- c(ylabel1, ylabel2)
@@ -324,14 +321,14 @@ g_waterfall <- function(bar_id,
         ylab(y_label)
     }
 
-    if (show_datavalue == TRUE){
+    if (show_datavalue == TRUE) {
       p <- p + geom_text(
         label = format(bar_data$bar_height, digits = 1),
         size = 2.5,
         vjust = ifelse(bar_data$bar_height >= 0, -0.5, 1.5))
     }
 
-    if (!is.null(add_label)){
+    if (!is.null(add_label)) {
       p <- p + geom_text(
         aes(x = bar_id, y = 0, label = bar_data$add_label),
         size = 3,
@@ -347,7 +344,7 @@ g_waterfall <- function(bar_id,
           legend.text = element_text(size = 9),
           legend.key = element_rect(fill = NA)
         )
-      if (!is.null(bar_color_opt)){
+      if (!is.null(bar_color_opt)) {
         p <- p + scale_fill_manual(values = bar_color_opt)
       }
     }
@@ -362,13 +359,13 @@ g_waterfall <- function(bar_id,
       p <- p +
         labs(title = title) +
         theme(plot.title = element_text(face = "bold"))
-    }else{
+    } else {
       p <- p +
         labs(title = paste(title, "-", as.character(unique(facet_by)))) +
         theme(plot.title = element_text(face = "bold"))
     }
 
-    if (!is.null(anno_txt)){
+    if (!is.null(anno_txt)) {
       t_anno <- data.frame(
         bar_id,
         bar_height,
@@ -382,7 +379,7 @@ g_waterfall <- function(bar_id,
           t_anno$bar_id,
           levels = unique(t_anno$bar_id[order(t_anno$sort_by, t_anno$bar_height, decreasing = TRUE)])
         )
-      } else{
+      } else {
         t_anno$bar_id <- factor(
           t_anno$bar_id,
           levels = unique(t_anno$bar_id[order(t_anno$bar_height, decreasing = TRUE)])
@@ -404,7 +401,7 @@ g_waterfall <- function(bar_id,
 
       tb <- tableGrob(
         t_anno,
-        rows = NULL, #row.names(t_anno),
+        rows = NULL,
         cols = NULL,
         theme = my_theme)
 
@@ -432,7 +429,7 @@ g_waterfall <- function(bar_id,
         r = g1$layout[g1$layout$name == "panel", 4])
       g3 <- gtable_add_cols(g2, tb_rowname$widths, pos = 0)
       g <- gtable_add_grob(g3, tb_rowname, t = -1, l = 2)
-    }else{
+    } else {
       p <- p +
         theme(axis.title.x = element_text()) +
         labs(x = "Unique Subject ID")
@@ -441,7 +438,7 @@ g_waterfall <- function(bar_id,
     g
   }
 
-  if (is.null(facet_by)){
+  if (is.null(facet_by)) {
     gt <- facet_plot(
       bar_id = bar_id,
       bar_height = bar_height,
@@ -459,21 +456,22 @@ g_waterfall <- function(bar_id,
       title = title
     )
 
-  }else{
+  } else {
     facet_by <- factor(facet_by)
     g_list <- rep(list(NA), length(levels(facet_by)))
-    for (i in 1:length(levels(facet_by))){
+    for (i in seq_along(levels(facet_by))) {
+      facet_level <- levels(facet_by)[i]
       g_list[[i]] <- facet_plot(
-        bar_id = bar_id[facet_by == levels(facet_by)[i]],
-        bar_height = bar_height[facet_by == levels(facet_by)[i]],
-        sort_by = sort_by[facet_by == levels(facet_by)[i]],
-        col_by = col_by[facet_by == levels(facet_by)[i]],
+        bar_id = bar_id[facet_by == facet_level],
+        bar_height = bar_height[facet_by == facet_level],
+        sort_by = sort_by[facet_by == facet_level],
+        col_by = col_by[facet_by == facet_level],
         bar_color_opt = bar_color_opt,
-        anno_txt = anno_txt[facet_by == levels(facet_by)[i], ],
+        anno_txt = anno_txt[facet_by == facet_level, , drop = FALSE],
         href_line = href_line,
-        facet_by = facet_by[facet_by == levels(facet_by)[i]],
+        facet_by = facet_by[facet_by == facet_level],
         show_datavalue = show_datavalue,
-        add_label = add_label[facet_by == levels(facet_by)[i]],
+        add_label = add_label[facet_by == facet_level],
         gap_point = gap_point,
         ytick_at = ytick_at,
         y_label = y_label,
