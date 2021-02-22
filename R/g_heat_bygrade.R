0618f20e0c16ea92ab6f4c591342196050ec944c
hey59 <hey59@gene.com>
2021-02-22 22:00:36 +0000

    add conmed shape

--- R/g_heat_bygrade.R
+++ R/g_heat_bygrade.R
@@ -10,6 +10,10 @@
 #' Usually is \code{ADCM}.
 #' @param heat_color_var (`character`)\cr
 #' @param heat_color_opt (`vector`)\cr
+#' @param conmed_data (`dataframe`)\cr
+#' @param conmed_var (`character`)\cr
+#' @param xlab (`character`)\cr
+#' @param title (`character`)\cr
 #' @import ggplot2
 #' @importFrom stringr str_to_upper
 #' @importFrom tidyr replace_na
@@ -26,8 +30,14 @@
 #' ADEX <- radex(ADSL)
 #' ADAE <- radae(ADSL)
 #' ADCM <- radcm(ADSL)
-#' #add AVISIT in ADAE
+#' # add AVISIT in ADAE
 #' ADAE$AVISIT <- sample(unique(ADEX$AVISIT[!is.na(ADEX$AVISIT)]), nrow(ADAE), TRUE)
+#' # include three conmed levels
+#' ADCM <- ADCM %>%
+#'   filter(
+#'     CMDECOD == "medname A_1/3" | CMDECOD == "medname A_2/3" | CMDECOD == "medname A_3/3"
+#'     ) %>%
+#'   mutate(CMDECOD = factor(CMDECOD, levels = unique(CMDECOD)))
 #' exp_data <- ADEX %>% filter(PARCAT1 == "INDIVIDUAL")
 #' anno_data <- ADSL
 #' anno_var <- c("SEX","COUNTRY")
@@ -36,6 +46,9 @@
 #' heat_color_name <- "Highest grade of\nindividual events"
 #' heat_color_opt <- c("0" = "gray90","1" = "lightsteelblue1", "2" = "steelblue1", "3" = "steelblue4",
 #'                     "4" = "maroon", "5" = "brown4")
+#' conmed_data <- ADCM
+#' conmed_var <- "CMDECOD"
+#'
 #' xlab = "Visit"; title = "Heatmap by Grade"
 #' g_heat_bygrade(
 #'   exp_data,
@@ -43,7 +56,9 @@
 #'   anno_var,
 #'   heat_data,
 #'   heat_color_var,
-#'   heat_color_opt
+#'   heat_color_opt,
+#'   conmed_data,
+#'   conmed_var
 #'   )
 g_heat_bygrade <- function(exp_data,
                            anno_data,
@@ -51,11 +66,26 @@ g_heat_bygrade <- function(exp_data,
                            heat_data,
                            heat_color_var,
                            heat_color_opt,
+                           conmed_data,
+                           conmed_var,
                            xlab = "Visit",
-                           title = "Heatmap by Grade"){
+                           title = "Heatmap by Grade") {
   # check if all PARCAT1 in exp_data is "individual"
   stop_if_not(
-    list(!is.na(exp_data$AVISIT), "Please only include 'INDIVIDUAL' record in exp_data")
+    list(is.data.frame(exp_data)),
+    list(!is.na(exp_data$AVISIT), "invalid argument: please only include 'INDIVIDUAL' record in exp_data"),
+    list(is.data.frame(anno_data)),
+    list(all(anno_var %in% names(anno_data)), "invalid argument: anno_var should be variable(s) from anno_data"),
+    list(is.data.frame(heat_data)),
+    list(
+      is_character_single(heat_color_var) && heat_color_var %in% names(heat_data),
+      "invalid argument: heat_color_var should be a varaible name in heat_data"
+      ),
+    list(is_character_vector(heat_color_opt)),
+    list(is.data.frame(conmed_data)),
+    list(
+      length(levels(conmed_data[[conmed_var]])) <= 3,
+      "invalid argument: please only include no more than three conmeds for plotting")
   )
 
   heat_data <- heat_data %>%
@@ -95,7 +125,7 @@ g_heat_bygrade <- function(exp_data,
       ) %>%
     select(USUBJID, SUBJ, AVISIT, RANK, AVAL, LASTDOSE, DOSERED) %>%
     filter(DOSERED == "Y")
-  #does ongoing data
+  # does ongoing data
   exp_lst <- exp_data %>%
     filter(PARAMCD == "DOSE") %>%
     filter(str_to_upper(EOSSTT) == "ONGOING") %>%
@@ -103,15 +133,27 @@ g_heat_bygrade <- function(exp_data,
     slice_tail() %>%
     select(USUBJID, SUBJ, AVISIT)
 
+  conmed_data <- conmed_data %>%
+    left_join(anl_data, by = "USUBJID") %>%
+    mutate(conmed_n = as.numeric(CMDECOD)) %>%
+    mutate(distance = (conmed_n - median(unique(conmed_n), na.rm = TRUE))/5) %>%
+    mutate(conmed_x = as.numeric(factor(AVISIT, levels = visit_levels)) + distance) %>%
+    group_by(USUBJID) %>%
+    mutate(SUBJ = tail(strsplit(USUBJID, "-")[[1]], n = 1)) %>%
+    select(USUBJID, SUBJ, conmed_x, !!conmed_var)
 
   subj_levels <- unique(anl_data$SUBJ)
   visit_levels <- unique(anl_data$AVISIT)
-  p <- ggplot(data = anl_data, aes(x = AVISIT,
-                                   y = factor(SUBJ, levels = c(subj_levels, "")))) +
+  p <- ggplot(
+    data = anl_data,
+    aes(x = AVISIT, y = factor(SUBJ, levels = c(subj_levels, "")))
+    ) +
     geom_tile(aes(fill = heat_color_max)) +
     scale_y_discrete(drop = FALSE) +
-    scale_fill_manual(name = heat_color_name,
-                      values = heat_color_opt) +
+    scale_fill_manual(
+      name = heat_color_name,
+      values = heat_color_opt
+      ) +
     # plot dose reduction
     geom_segment(
       data = ex_red,
@@ -121,19 +163,31 @@ g_heat_bygrade <- function(exp_data,
         yend = as.numeric(factor(SUBJ, levels = rev(subj_levels))) - 0.3,
         xend = AVISIT
         ),
-      arrow = arrow(length = unit(0.1, "cm")), size = .5, color = "black"
+      arrow = arrow(length = unit(0.1, "cm")),
+      size = .5,
+      color = "black"
       ) +
     # plot ongoing
-    geom_segment(data = exp_lst,
-                 aes(x = as.numeric(factor(AVISIT, levels = visit_levels)) + 0.5,
-                     xend = as.numeric(factor(AVISIT, levels = visit_levels)) + 0.65,
-                     y = as.numeric(factor(SUBJ, levels = rev(subj_levels))),
-                     yend = as.numeric(factor(SUBJ, levels = rev(subj_levels)))),
-                 arrow = arrow(length = unit(0.1, "cm")), size = .5, color = "black") +
-    # # if there's atezo IRR, add in text
-    # geom_text(aes(x = as.numeric(factor(AVISITCD, levels = visit_levels)) + 0.2,
-    #               label = ATEZOEXP),
-    #           color = "white", size = 2, fontface = "bold", na.rm = T) +
+    geom_segment(
+      data = exp_lst,
+      aes(x = as.numeric(factor(AVISIT, levels = visit_levels)) + 0.5,
+          xend = as.numeric(factor(AVISIT, levels = visit_levels)) + 0.65,
+          y = as.numeric(factor(SUBJ, levels = rev(subj_levels))),
+          yend = as.numeric(factor(SUBJ, levels = rev(subj_levels)))
+          ),
+      arrow = arrow(length = unit(0.1, "cm")),
+      size = .5,
+      color = "black"
+      ) +
+    # plot conmed
+    geom_point(
+      data = conmed_data,
+      aes(
+        x = conmed_x,
+        y = as.numeric(factor(SUBJ, levels = rev(subj_levels))),
+        shape = conmed_data[[conmed_var]]
+        )
+      ) +
     theme_bw() +
     theme(
       panel.background = element_blank(),
@@ -143,10 +197,7 @@ g_heat_bygrade <- function(exp_data,
       axis.text.y = element_blank(),
       axis.title.y = element_blank()
     ) +
-    labs(
-      x = xlab,
-      y = "ylab"
-     )
+    labs(x = xlab, y = "ylab")
   # plot title and labels
   if (!is.null(title)) {
     p <- p +
