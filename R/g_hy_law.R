7867d4b4ac5ff89bbde54612c7a29f2b09d5a486
Withycombe, Katie {MDBA~Welwyn} <katie.withycombe@roche.com>
2021-07-23 11:54:59 +0200

    add axes, title and caption

--- R/g_hy_law.R
+++ R/g_hy_law.R
@@ -17,3 +17,33 @@ adlb <- radlb(adsl) %>%
   left_join(anrhi) %>%
   mutate(ULN = MAX/ANRHI) %>%
   pivot_wider(id_cols = USUBJID, names_from = PARAMCD, values_from = ULN)
+
+
+ggplot(data = adlb) +
+
+  scale_x_continuous(
+    name = "Maximum Alanine Aminotransferase (/ULN)",
+    breaks = log10(c(seq(0.1, 1, 0.1), seq(2, 10, 1), seq(20, 100, 10))),
+    limits = c(-1, 2),
+    labels = c(0.1, rep(" ", 8), 1, rep(" ", 8), 10, rep(" ", 8), 100),
+    expand = c(0.01,0.01)
+  ) +
+  scale_y_continuous(
+    name = "Maximum Total Bilirubin (/ULN)",
+    breaks = log10(c(seq(0.1, 1, 0.1), seq(2, 10, 1), seq(20, 100, 10))),
+    limits = c(-1, 2),
+    labels = c(0.1, rep(" ", 8), 1, rep(" ", 8), 10, rep(" ", 8), 100),
+    expand = c(0.01,0.01)
+  ) +
+
+  labs(title = "Scatter Plot of Maximum Total Bilirubin versus Maximum Alanine Aminotransferase",
+       caption = "Maximum values are those maximum values that occur post-baseline (no time constraints and not necessarily concurrent events).") +
+
+  theme_bw(base_size = 14, base_family = "Arial") +
+
+  theme(plot.title = element_text(hjust = 0.5),
+        plot.title.position = "plot",
+        plot.caption = element_text(hjust = 0),
+        plot.caption.position = "plot",
+        panel.grid = element_blank())
+
