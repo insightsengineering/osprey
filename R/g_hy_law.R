e66934094e998a784855f177cf0d60cca4df4361
Franklin, Amy {MDBT~Welwyn} <amy.franklin@roche.com>
2021-07-23 12:28:25 +0200

    Added colours and shapes

--- R/g_hy_law.R
+++ R/g_hy_law.R
@@ -9,6 +9,8 @@ library(tidyr)
 anrhi <- data.frame(ANRHI = c(50,50), PARAMCD = c("ALT","CRP"))
 
 adsl <- radsl(N=30)
+arm <- adsl %>%
+  select(USUBJID,ARM)
 adlb <- radlb(adsl) %>%
   filter(AVISITN>0 & PARAMCD %in% c("CRP","ALT")) %>%
   group_by(USUBJID,PARAMCD) %>%
@@ -16,4 +18,9 @@ adlb <- radlb(adsl) %>%
   slice(1) %>%
   left_join(anrhi) %>%
   mutate(ULN = MAX/ANRHI) %>%
-  pivot_wider(id_cols = USUBJID, names_from = PARAMCD, values_from = ULN)
+  pivot_wider(id_cols = USUBJID, names_from = PARAMCD, values_from = ULN) %>%
+  left_join(arm)
+
+
+a <- ggplot(adlb, aes(x = ALT, y = CRP))
+a + geom_point(aes(shape=ARM, color = ARM)) + scale_shape_manual(values = c(1:3))
