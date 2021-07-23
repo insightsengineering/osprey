208f185e8c80c2a7e3a304b5bac38fd69d2fd022
Holmes, William {MDBD~Welwyn} <william.holmes@roche.com>
2021-07-23 12:14:02 +0200

    Added inner graph text and lines

--- R/g_hy_law.R
+++ R/g_hy_law.R
@@ -17,3 +17,49 @@ adlb <- radlb(adsl) %>%
   left_join(anrhi) %>%
   mutate(ULN = MAX/ANRHI) %>%
   pivot_wider(id_cols = USUBJID, names_from = PARAMCD, values_from = ULN)
+
+p <- ggplot(data = adlb) +
+  geom_point(aes(x = log(ALT), y = log(CRP))) +
+    scale_x_continuous(
+    name = "Peak ALT, xULN",
+    breaks = log10(c(seq(0.1, 1, 0.1), seq(2, 10, 1), seq(20, 100, 10))),
+    limits = c(-1, 2),
+    labels = c(0.1, rep(" ", 8), 1, rep(" ", 1), 3, rep(" ", 6), 10, rep(" ", 8), 100)
+  ) +
+  scale_y_continuous(
+    name = "Peak TBILI, xULN",
+    breaks = log10(c(seq(0.1, 1, 0.1), seq(2, 10, 1), seq(20, 100, 10))),
+    limits = c(-1, 2),
+    labels = c(0.1, rep(" ", 8), 1, 2, rep(" ", 7), 10, rep(" ", 8), 100)
+  ) +
+  geom_segment(
+    aes(x = log10(3), y = log10(0), xend = log10(3), yend = log10(85)),
+    size = 0.25,
+    color = "grey"
+  ) +
+  geom_segment(
+    aes(x = log10(0), y = log10(2), xend = log10(80), yend = log10(2)),
+    size = 0.25,
+    color = "grey"
+  ) +
+  geom_segment(
+    aes(x = log10(1), y = log10(0), xend = log10(1), yend = log10(1)),
+    size = 0.25,
+    color = "black"
+  ) +
+  geom_segment(
+    aes(x = log10(0), y = log10(1), xend = log10(1), yend = log10(1)),
+    size = 0.25,
+    color = "black"
+  ) +
+  annotate("text", label = "3XULN", x = log10(3), y = log10(100)) +
+  annotate("text", label = "2XULN", x = log10(100), y = log10(2)) +
+  annotate("text", label = "Hyperbilirubinemia", x = log10(0.2), y = log10(80)) +
+  annotate("text", label = "Possible Hy's Law Range", x = log10(40), y = log10(80)) +
+  annotate("text", label = "Normal Range", x = log10(0.2), y = log10(0.1)) +
+  annotate("text", label = "Temple's Corollary", x = log10(40), y = log10(0.1)) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank())
+
+
+
