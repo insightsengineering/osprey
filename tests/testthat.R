99d7e27ae8d3c5ad8fd9d3517394c69a9a7e4700
Dinakar <26552821+dinakar29@users.noreply.github.com>
2021-09-24 17:04:20 -0500

    Enhance CI workflows (#32)

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,10 @@
-library(testthat)
-library(scda) # nolint
-
-test_check("osprey")
+pkg_name <- "osprey"
+if (requireNamespace("testthat", quietly = TRUE)) {
+  library(testthat)
+  reporter <- MultiReporter$new(list(
+    CheckReporter$new(),
+    JunitReporter$new(file = "junit-result.xml")
+  ))
+  test_results <- test_check(pkg_name, reporter = reporter)
+  saveRDS(test_results, "unit_testing_results.rds")
+}
